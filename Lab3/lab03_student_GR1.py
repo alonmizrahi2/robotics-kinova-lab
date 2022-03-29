8e10aac156290b6978b12cee448f4c5d83fa0620
alonmizrahi2 <alonmizrahi2@mail.tau.ac.il>
2022-03-29 16:11:18 +0300

    29_3_a

--- Lab3/lab03_student_GR1.py
+++ Lab3/lab03_student_GR1.py
@@ -49,19 +49,23 @@ def dh(alpha, a, d, theta):
                      [0, sin(alpha), cos(alpha), d],
                      [0, 0, 0, 1]])
 
-def transform_matrices():
+def T_mat():
 
     dictionary = set_dh_table()
+    Tes = Matrix([[0, -1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
     T_01 = dh(dictionary[alpha1], dictionary[a1], dictionary[d1], dictionary[q1])
     T_12 = dh(dictionary[alpha2], dictionary[a2], dictionary[d2], dictionary[q2])
+    T_02 = T_01*T_12*Tes
     T_23 = dh(dictionary[alpha3], dictionary[a3], dictionary[d3], dictionary[q3])
+    T_03 = T_01*T_12*T_23*Tes
     T_34 = dh(dictionary[alpha4], dictionary[a4], dictionary[d4], dictionary[q4])
+    T_04 = T_01*T_12*T_23*T_34*Tes
     T_45 = dh(dictionary[alpha5], dictionary[a5], dictionary[d5], dictionary[q5])
+    T_05 = T_01*T_12*T_23*T_34*T_45*Tes
     T_56 = dh(dictionary[alpha6], dictionary[a6], dictionary[d6], dictionary[q6])
-    Tes = Matrix([[0, -1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-    T = T_01 * T_12 * T_23 * T_34 * T_45 * T_56 * Tes
 
-    return T
+    T = T_01 * T_12 * T_23 * T_34 * T_45 * T_56 * Tes
+    return [T_01,T_02,T_03,T_04,T_05,T]
 
 def FK(theta_list):
 
@@ -101,6 +105,7 @@ def FK(theta_list):
     return T_0G_eval
 
 #
+
 def Jacobian(Q):
     '''
 
@@ -110,8 +115,42 @@ def Jacobian(Q):
     Returns:
         Full Jacobian matrix [6,6]
     '''
+    T = T_mat()
+    Te = T[-1]
+    ## calculating vector b_i1, b_i1 =Ri-1*bz
+    R01 = T[0][:3,0:3]
+    R02 = T[1][:3,0:3]
+    R03 = T[2][:3,0:3]
+    R04 = T[3][:3,0:3]
+    R05 = T[4][:3,0:3]
+    bz = Matrix([[0],[0],[1]])
+    b_01 = R01 * bz
+    b_02 = R02 * bz
+    b_03 = R03 * bz
+    b_04 = R04 * bz
+    b_05 = R05 * bz
+    ## calculating the vector r_ie, r_ie = d_0e - d_ie
+    d_0e = T[-1][:3, -1]
+    d_1e = T[0][:3, -1]
+    d_2e = T[1][:3, -1]
+    d_3e = T[2][:3, -1]
+    d_4e = T[3][:3, -1]
+    d_5e = T[4][:3, -1]
+    r_0e = d_0e
+    r_1e = d_0e - d_1e
+    r_2e = d_0e - d_2e
+    r_3e = d_0e - d_3e
+    r_4e = d_0e - d_4e
+    r_5e = d_0e - d_5e
 
-    pass
+    Jl = Matrix([[bz.cross(r_0e),b_01.cross(r_1e),b_02.cross(r_2e),b_03.cross(r_3e),b_04.cross(r_4e),b_05.cross(r_5e)]])
+    Ja = Matrix([[bz,b_01,b_02,b_03,b_04,b_05]])
+    J = Matrix([[Jl],[Ja]])
+    
+    theta_dict = {q1:Q[0], q2:Q[1], q3:Q[2], q4:Q[3], q5:Q[4], q6:Q[5]}
+    J_eval = J.evalf(subs=theta_dict, chop=True, maxn=4)
+    J = np.matrix(J_eval).astype(np.float64)
+    return J
 
 def LinearJacobian(Q):
     '''
@@ -122,8 +161,9 @@ def LinearJacobian(Q):
     Returns:
         Linear part of the Jacobian matrix [3,6]
     '''
-
-    pass
+    J = Jacobian(Q)
+    Jl = J[0:3,:]
+    return Jl
 
 def IK_NR_position(guess, target):
     '''
@@ -135,4 +175,38 @@ def IK_NR_position(guess, target):
     Returns:
         Q* - joint configuration angles [1, 6]
     '''
-    pass
+    th0 = np.array(guess)
+    xd = np.array(target)
+    i = 0
+    eps = 0.01 ## minimum error
+    xd_tamp = np.array(FK(th0)).astype(np.float64)
+    xd_tamp = xd_tamp[:3,3].transpose()
+    e = xd - xd_tamp
+    e = np.linalg.norm(e)
+    lamb = 0.1
+    while e > eps:
+        Jl = np.array(LinearJacobian(th0))
+        Jl_pi = np.linalg.pinv(Jl)
+        th0 = th0 +lamb * Jl_pi.dot(xd - xd_tamp)
+        th0 = np.array(th0)
+        i = i + 1
+        xd_tamp = np.array(FK(th0)).astype(np.float64)
+        xd_tamp = xd_tamp[:3,3].transpose()
+        e = xd - xd_tamp
+        e = np.linalg.norm(e)
+        print(f'iter: {i}, Error: {e} \tAngles: {np.rad2deg(th0)}')
+    Q = th0
+    print(f'Finel Error: {e}')
+    return Q
+
+## main
+
+target_1 = [0.2, 0.5, 0.1]
+guess_1 = [np.deg2rad(270),np.deg2rad(148) , np.deg2rad(148), np.deg2rad(270), np.deg2rad(140),np.deg2rad(0)]
+print(f'guess = {guess_1}')
+Q_1 = IK_NR_position(guess_1, target_1)
+print(f'Actuators Angles: {np.rad2deg(Q_1)}')
+
+target_real_FK = np.array(FK(Q_1)).astype(np.float64)
+target_real_FK = target_real_FK[:3,3].transpose()
+print(f'FK for check: {target_real_FK}')
