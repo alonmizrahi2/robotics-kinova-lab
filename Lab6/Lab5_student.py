d1d2694aef8a046d88a30cc28b5749284351ddb6
alonmizrahi2 <alonmizrahi2@mail.tau.ac.il>
2022-05-18 11:50:12 +0300

    rrt_star

--- Lab6/Lab5_student.py
+++ Lab6/Lab5_student.py
@@ -1,5 +1,7 @@
 # Import required packages
-from rrt_algo import *
+# from rrt_algo import *
+from rrt_star_algo import *
+import numpy as np
 
 
 def planner(Pc, Pg, O, B , delta=0.02, **args): #B=[-0.05, 0.65]
@@ -21,15 +23,36 @@ def planner(Pc, Pg, O, B , delta=0.02, **args): #B=[-0.05, 0.65]
 
     # ====Search Path with RRT====
 
-    # Set Initial parameters
-    rrt = RRT(
+    # # Set Initial parameters -rrt regular
+    # rrt = RRT(
+    #     start=Pc, goal=Pg, rand_area=B,
+    #     obstacle_list=O,
+    #     path_resolution=delta,
+    #     # play_area=[0, 10, 0, 14]
+    #     robot_radius=0.005
+    #     )
+    # path = rrt.planning(animation=True) # True or False
+
+    # if path is None:
+    #     print("Cannot find path")
+    # else:
+    #     print("found path!!")
+
+    #     # Draw final path
+    #     if show_animation:
+    #         rrt.draw_graph()
+    #         plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
+    #         plt.grid(True)
+    #         plt.show()
+    #         # save the fig ??----------------------------------
+
+    # Set Initial parameters - rrt star
+    rrt_star = RRTStar(
         start=Pc, goal=Pg, rand_area=B,
         obstacle_list=O,
-        path_resolution=delta,
-        # play_area=[0, 10, 0, 14]
-        robot_radius=0.005
-        )
-    path = rrt.planning(animation=True) # True or False
+        expand_dis=0.05,
+        robot_radius=0.005)
+    path = rrt_star.planning(animation=show_animation)
 
     if path is None:
         print("Cannot find path")
@@ -38,11 +61,10 @@ def planner(Pc, Pg, O, B , delta=0.02, **args): #B=[-0.05, 0.65]
 
         # Draw final path
         if show_animation:
-            rrt.draw_graph()
-            plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
+            rrt_star.draw_graph()
+            plt.plot([x for (x, y) in path], [y for (x, y) in path], 'r--')
             plt.grid(True)
             plt.show()
-            # save the fig ??----------------------------------
 
     return path
 
@@ -72,28 +94,28 @@ def steering_angle(A_cam_robot, A_cam_base, p_i_base):
 
 if __name__ == '__main__':
     
-    # obstacleList = [(0.1, 0.1, 0.04), (0.1, 0.3, 0.04), (0.4, 0.6, 0.07)]  # [x, y, radius]
-    # start=[0, 0]
-    # goal=[0.09, 0.4]
-    # rand_area=[-0.05, 0.65]
+    obstacleList = [(0.1, 0.1, 0.04), (0.1, 0.3, 0.04), (0.4, 0.6, 0.07)]  # [x, y, radius]
+    start=[0, 0]
+    goal=[0.09, 0.4]
+    rand_area=[-0.05, 0.65]
 
-    # pp = planner(start, goal, obstacleList, rand_area, delta=0.02)
-    # pp2 = np.round(pp[::-1],3)
+    pp = planner(start, goal, obstacleList, rand_area, delta=0.02)
+    pp2 = np.round(pp[::-1],3)
 
-    # print(pp2)
+    print(pp2)
 
     # for check
-    A_robot_cam = np.array([[-0.1614, -0.6982, 0.6975, 0.412],
-                            [0.9769, -0.0127, 0.2133, 0.218],
-                            [-0.1400, 0.7158, 0.6841, 0.797],
-                            [0., 0., 0., 1.]])
-    A_base_cam = np.array([[-0.7537, 0.6208, 0.2157, 0.112],
-                           [-0.1910, -0.5292, 0.8246, 0.801],
-                           [0.6261, 0.5783, 0.5230, 0.797],
-                           [0., 0., 0., 1.]])
-    p_i_base = np.array([0.5, 1.1, 0.]) 
+    # A_robot_cam = np.array([[-0.1614, -0.6982, 0.6975, 0.412],
+    #                         [0.9769, -0.0127, 0.2133, 0.218],
+    #                         [-0.1400, 0.7158, 0.6841, 0.797],
+    #                         [0., 0., 0., 1.]])
+    # A_base_cam = np.array([[-0.7537, 0.6208, 0.2157, 0.112],
+    #                        [-0.1910, -0.5292, 0.8246, 0.801],
+    #                        [0.6261, 0.5783, 0.5230, 0.797],
+    #                        [0., 0., 0., 1.]])
+    # p_i_base = np.array([0.5, 1.1, 0.]) 
 
-    # p_i_car = ([0,0])
-    (p_i_car, alpha) = steering_angle(A_robot_cam, A_base_cam, p_i_base)
+    # # p_i_car = ([0,0])
+    # (p_i_car, alpha) = steering_angle(A_robot_cam, A_base_cam, p_i_base)
     
-    print(p_i_car, alpha)
+    # print(p_i_car, alpha)
