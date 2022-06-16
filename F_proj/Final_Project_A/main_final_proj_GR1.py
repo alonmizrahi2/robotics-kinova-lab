d00aac94c8399779816bef7ea317078af223be29
alonmizrahi2 <alonmizrahi2@mail.tau.ac.il>
2022-06-16 14:28:49 +0300

    final_final

--- F_proj/Final_Project_A/main_final_proj_GR1.py
+++ F_proj/Final_Project_A/main_final_proj_GR1.py
@@ -23,23 +23,13 @@ sys.path.insert(0, r'../common/Aruco_Tracker-master')
 from aruco_module import aruco_track
 
 
-def planner(Pc, Pg, O, planner_type , B=[-0.05, 0.65] , delta=0.02, **args): #B=[-0.05, 0.65]
+def planner(Pc, Pg, O, planner_type , B=[-0.05, 0.65] , delta=0.02, **args): 
     """
-    Args:
-        Pc: start point (x_s, y_s) --> list: len=2 OR np.array(): shape=(2,)
-        Pg: end point (x_g, y_g) --> list: len=2 OR np.array(): shape=(2,)
-        O: [(x_obs_1, y_obs_2, radius_obs_1), ..., (x_obs_N, y_obs_N, radius_obs_N)
-        B: this is the area where you plan the path in. both x and y should be in between these boundaries.
-        delta: Path resolution.
-        **args: add additional arguments as you please such as whether to plot a path or not.
-
-    Returns:
-        path: [[x_1, y_1], [x_2, y_2], ..., [x_M, y_M]] --> List of lists of size 2 (x, y).
-                Important: Make sure that your output 'path' is in the right order (from start to goal)
+    planner - plane path with 5 options
     """
     print("start " + __file__)
 
-    # RRT == 1, RRT* == 2, POTENTIAL == 3, STRAIGHT LINE == 4
+    # RRT == 1, RRT* == 2, POTENTIAL == 3, STRAIGHT LINE == 5
 
     if(planner_type == 1):
     # ====Search Path with RRT====
@@ -118,36 +108,19 @@ def planner(Pc, Pg, O, planner_type , B=[-0.05, 0.65] , delta=0.02, **args): #B=
         
     return path[::-1]
 
-def steering_angle(A_cam_base, A_cam_robot, p_i_base):
-    """
-    Args:
-        A_robot_cam: Homogeneous matrix from car to camera frame
-        A_base_cam: Homogeneous matrix from origin to camera frame
-        p_i_base: 2d vector of next point in the path with respect to base (origin) frame: (x, y)
-
-    Returns:
-        p_i_car: 2d vector of next point in path with respect to car frame: (x, y)
-        alpha: Steering angle to next point [degrees].
-    """
-
-    A_robot_cam = np.round(np.linalg.inv(A_cam_robot),4)
-    A_robot_base = np.round(np.matmul(A_robot_cam, A_cam_base),4)
-    p_i_base = np.hstack((np.array(p_i_base), np.array([0,1])))
-    p_i_car = np.round(np.dot(A_robot_base, p_i_base),3)
-
-    p_i_car_f = p_i_car[:-2]
-    alpha = np.round(np.rad2deg(np.arctan2(p_i_car_f[0], p_i_car_f[1])), 2)
-    return (p_i_car_f, alpha)
-
 def attack_point_calc(p_d,delta = 0.10):
-    #notice!! side need to be defined as global parameter -1/1
+    """
+    calculate attack point
+    """
     side = 1
     p_delta = np.array([side*delta,0])
     p_attack = p_d - p_delta
     return p_attack
 
 def goal_point(p_d ,p_own_corrent, step_size, side):
-
+    """
+    calculate goal point
+    """
     side = 1
     p_delta = (p_d - p_own_corrent)*step_size*side
     goal_point = p_own_corrent + p_delta
@@ -155,13 +128,18 @@ def goal_point(p_d ,p_own_corrent, step_size, side):
 
 
 def static_target(p_previous , pcur ,  max_error = 0.2):
-    #notice!! at the end (or start) of the main code we must define the p_previous of the target
+    """
+    check if the disk move
+    """
     bool_parameter = True
     if abs(pcur[0]-p_previous[0]) > max_error or abs(pcur[1]-p_previous[1]) > max_error:
         bool_parameter = False       
     return bool_parameter
 
 def close_to_disk(p_d,p_o,tol = 0.10):
+    """
+    check if own robot or rival robot close to the disk
+    """
     d = np.linalg.norm(p_o - p_d)
     if d > tol:
         return False
@@ -169,6 +147,10 @@ def close_to_disk(p_d,p_o,tol = 0.10):
         return True
 
 def GoToNextPoint(p_own,point, tolerance = 0.01):
+    """
+    move own robot to the next point
+    loop that give order to the motors and check if the robot reach the next point
+    """
     next_ = [10e2, 10e2]
     while np.linalg.norm(next_[:2]) > tolerance:
         
@@ -199,32 +181,9 @@ def GoToNextPoint(p_own,point, tolerance = 0.01):
     print("Reached next point")
     cntrlr.motor_command(1., 1.)
 
-def save(saver):
-    logdir_prefix = 'lab-05'
-
-    data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../Lab5/data')
-
-    if not (os.path.exists(data_path)):
-        os.makedirs(data_path)
-
-    logdir = logdir_prefix + '_' + time.strftime("%d-%m-%Y_%H-%M-%S")
-    logdir = os.path.join(data_path, logdir)
-    if not (os.path.exists(logdir)):
-        os.makedirs(logdir)
-
-    print("\n\n\nLOGGING TO: ", logdir, "\n\n\n")
-
-    import pickle
-    with open(logdir + '/data' + '.pkl', 'wb') as h:
-        pickle.dump(saver, h)
-
 def calc_motor_command(angle):
     '''
-    Args:
-        angle: steering angle from car pose to target pose
-
-    Returns:
-        left and right motor command to align with the steering angle
+    calculate motor command
     '''
     x = angle / 180.
     if x <= 0:
@@ -239,9 +198,7 @@ def calc_motor_command(angle):
 
 def field_status():
     '''
-    w.r.t camera frame
-    Returns:
-
+    receive pose in base coordinate of own robot, rival robot and the disk
     '''
     t_curr, R_curr, ids = tracker.track()
     try:
@@ -262,12 +219,13 @@ def field_status():
         cntrlr.motor_command(1., 1.)
 
 def calc_xy_base_vector(mat1, mat2, mat3):
+    '''
+    receive (x,y) positions in base coordinate of own robot, rival robot and the disk from pose
+    '''
     p1 = np.array([mat1[0,3],mat1[1,3]])
     p2 = np.array([mat2[0,3],mat2[1,3]])
     p3 = np.array([mat3[0,3],mat3[1,3]])
     return p1, p2, p3
-
-
 
 ####################
 #### main loop #####
@@ -275,7 +233,7 @@ def calc_xy_base_vector(mat1, mat2, mat3):
 if __name__ == "__main__":
 
     tracker = aruco_track()
-
+    ## constant for each game
     axes_user_ID = 10# int(input('Enter axes user ID:   '))
     car_ID = 2#int(input('Enter car ID:   '))
     opponent_ID =3# int(input('Enter opponent ID:   '))
@@ -304,9 +262,9 @@ if __name__ == "__main__":
     goal_achived = True
     while goal_achived is True:
         to_target =True
-        while to_target  is True:
+        while to_target  is True: # loop to reach attack point
             p_attack = attack_point_calc(p_disk)
-            A_path = planner(p_own, p_attack, obs, planner_type , B=[-0.05, 0.65])   #### Plane path planner_type=  RRT == 1, RRT* == 2, POTENTIAL == 3, STRAIGHT LINE == 4
+            A_path = planner(p_own, p_attack, obs, planner_type , B=[-0.05, 0.65])   # Plane path planner_type=  RRT == 1, RRT* == 2, POTENTIAL == 3, STRAIGHT LINE == 5
             for point in A_path:
                 GoToNextPoint(p_own, point)
                 p_rival_mat, p_own_mat, p_disk_mat = field_status() # matrix
@@ -314,6 +272,7 @@ if __name__ == "__main__":
                 p_previous = pcur
                 pcur = p_disk
                 if static_target(p_previous,pcur)  is False:
+                    # check that the disk dont move
                     to_target = True
                     break
                 to_target = False
@@ -323,7 +282,7 @@ if __name__ == "__main__":
         goal_p = goal_point(p_disk, p_own, step_size = 5, side = 1)
         B_path = planner(p_own, goal_p, obs, planner_type , B=[-0.05, 0.65])   #### Plane path planner_type=  RRT == 1, RRT* == 2, POTENTIAL == 3, STRAIGHT LINE == 4
         to_goal = True
-        while to_goal  is True:
+        while to_goal  is True: # loop to reach goal
             for point in B_path:
                 GoToNextPoint(p_own, point)
                 p_rival_mat, p_own_mat, p_disk_mat = field_status() # matrix
@@ -341,9 +300,5 @@ print('Goallllllll!!!!!!')
     #######################
     #### Goallllllllll ####
     #######################
-
-    ########################
-    #### Yala Beitarrrr ####
-    ########################
 
 
